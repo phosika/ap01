6ec1ef66b9519dcc60ab0e53531546d535c0c930
phosika <58169224+phosika@users.noreply.github.com>
2025-01-22 15:44:58 +0700

    add import task to model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Model\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         Task::factory(10)->create();
+        TaskController
     }
 }
