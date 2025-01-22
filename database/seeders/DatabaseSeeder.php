43193e500f2c98450c5dde41a4200fc2381e466a
phosika <58169224+phosika@users.noreply.github.com>
2025-01-22 15:47:43 +0700

    model to models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Model\Task;
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
